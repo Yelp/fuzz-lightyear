6816142ff9354a544e7932b64449ef2b38caa884
Aaron Loo <aaronloo@yelp.com>
2019-09-19 12:08:59 -0700

    adding comment to explain input saving for stateful requests

--- fuzz_lightyear/runner.py
+++ fuzz_lightyear/runner.py
@@ -16,6 +16,12 @@ def run_sequence(
         )
 
         responses.add_response(response)
+
+        # The Response object already saves the output of the response.
+        # However, we also still want to save some of the *inputs* to the
+        # request, if they were factory-generated. This tries to ensure
+        # the same object is referenced throughout the sequence, rather than
+        # having the fuzzing engine constantly generate new objects.
         for key in request.fuzzed_input:
             if key in get_user_defined_mapping():
                 responses.data[key] = request.fuzzed_input[key]
