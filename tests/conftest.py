9734924f281b2b7e36dcbb561e2a28cbd0c91585
Aaron Loo <aaronloo@yelp.com>
2020-04-07 17:07:32 -0700

    less hacky filtering of hypothesis warnings in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,17 +31,15 @@ def clear_caches():
     _RERUN_POST_FUZZ_HOOKS_BY_TAG.clear()
 
 
-@pytest.fixture(autouse=True, scope='session')
-def trick_hypothesis():
+@pytest.fixture(autouse=True)
+def ignore_hypothesis_non_interactive_example_warning():
     """In theory we're not supposed to use hypothesis'
     strategy.example(), but fuzz-lightyear isn't using
     hypothesis in a normal way.
-
-    HACK: Thus, let's silence the warning that hypothesis
-    logs, making a lot easier to interpret fuzz-lightyear
-    test results.
-
-    Relevant hypothesis code:
-    https://github.com/HypothesisWorks/hypothesis/blob/83a98364d30b88085adf2a0d0a1f7ca1ba2d4ce5/hypothesis-python/src/hypothesis/strategies/_internal/strategies.py#L273
     """
-    sys.ps1 = ''
+    import warnings
+    from hypothesis.errors import NonInteractiveExampleWarning
+    warnings.filterwarnings(
+        'ignore',
+        category=NonInteractiveExampleWarning,
+    )
